18c4341f3e2f4c652cc1e8bfaaccc80beb3b36cd
Steve Schwarz <steve@agilitynerd.com>
2020-03-21 10:04:40 -0500

    Convert to node https.request

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,66 @@
 const proc = require('child_process')
+const https = require('https')
 
-exports.onPostBuild = function (_, pluginOptions) {
+const rollbarDeploy = (payload) => {
+  return new Promise((resolve, reject) => {
+
+    const options = {
+      hostname: 'api.rollbar.com',
+      port: 443,
+      path: '/api/1/deploy/',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': payload.length
+      }
+    }
+
+    const req = https.request(options, res => {
+      var data = []
+      res.on('data', d => {
+        data.push(d)
+      })
+      res.on('end', d => {
+        const result = JSON.parse(data.join(''))
+        resolve(result)
+      })
+    })
+
+    req.on('error', error => {
+      reject(error)
+    })
+    req.write(payload)
+    req.end()
+  })
+}
+
+exports.onPostBuild = async function (_, pluginOptions) {
+
   if (process.env.NODE_ENV === 'production') {
     const localUserName = pluginOptions.localUserName || 'gatsby'
+    const environment = pluginOptions.environment || process.env.NODE_ENV
 
     const revision = proc.execSync('git rev-parse --short HEAD').toString().trim()
 
-    const cmd = `curl --request POST --url https://api.rollbar.com/api/1/deploy/ \
---header 'content-type: application/json' \
--d '{"access_token": "${pluginOptions.accessToken}", "environment": "production", "revision": "${revision}", "local_username": "${localUserName}"}'`
+    const payload = JSON.stringify(
+      {
+        access_token: pluginOptions.accessToken,
+        environment: environment,
+        revision: revision,
+        local_username: localUserName
+      }
+    )
 
-    const result = proc.execSync(cmd, {
-      stdio: [0, 1, 2]
-    })
-    console.log("Rollbar deploy results:", result)
+    results = await rollbarDeploy(payload)
+    if (results.err) {
+      const msg = `Rollbar deploy error IGNORED: ${JSON.stringify(results)}`
+      if (pluginOptions.ignoreErrors) {
+        console.error(msg)
+      } else {
+        throw new Error(msg)
+      }
+    } else {
+      console.log("Rollbar Deploy Success: ", results)
+    }
   }
 }
